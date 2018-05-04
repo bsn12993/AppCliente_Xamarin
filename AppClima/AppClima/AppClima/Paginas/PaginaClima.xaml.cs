a36b689eecfebe21d92882f124023bb5ed2eb735
TASISOFTWARE\silverio_nieves <silverio_nieves@tasisoft.com>
2018-05-04 14:13:48 -0500

    Codigo inicial

---
+++ AppClima/AppClima/AppClima/Paginas/PaginaClima.xaml.cs
@@ -0,0 +1,37 @@
+﻿using AppClima.Clases;
+using AppClima.Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppClima.Paginas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PaginaClima : ContentPage
+	{
+		public PaginaClima ()
+		{
+			InitializeComponent ();
+            this.BindingContext = new Clima();
+		}
+
+        public async void BuscarClima_Clicked(object sender,EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtCiudad.Text))
+            {
+                var clima = await ServicioClima.ConsultarClima(txtCiudad.Text);
+                if (clima != null)
+                {
+                    this.BindingContext = clima;
+                    btnBuscar.Text = "Buscar de nuevo";
+                }
+            }
+        }
+
+    }
+}
